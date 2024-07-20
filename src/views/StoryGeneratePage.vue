f2340eb1ac83e129767014efdcd2198ba822d751
Zalven <zalven.dayao@go.team>
2024-07-20 15:05:08 +0800

    adding readme and notebook

--- src/views/StoryGeneratePage.vue
+++ src/views/StoryGeneratePage.vue
@@ -1,8 +1,8 @@
 <template>
   <MainLayout>
-    <div class="my-10 p-8">
+    <div class="my-10 p-8 bg-white backdrop-blur-sm">
       <p class="font-bold text-lg text-center w-full flex items-center justify-center">
-        <SparklesIcon class="w-5 mr-2" /> Generate Phil Iri Story
+        <SparklesIcon class="w-5 mr-2 " /> Generate Phil Iri Story
       </p>
       <p class="mt-5 text-xs text-center w-full">
         The Philippine Informal Reading Inventory (Phil-IRI) helps teachers assess students' reading skills. Our app
